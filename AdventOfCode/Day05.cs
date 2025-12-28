0ab499a278f65dc1ad370e6211173c222c261c96
Artuur-Oerlemans <artuur.oerlemans@gmail.com>
2025-12-28 16:24:44 +0100

    day 5

---
+++ AdventOfCode/Day05.cs
@@ -0,0 +1,150 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2025;
+
+public class Day05 : BaseDay
+{
+    private readonly List<string> _input;
+    private readonly Dictionary<(long, long), char> _dict;
+    private readonly List<long> _ids;
+    private readonly List<(long start, long end)> _freshRanges;
+    private readonly List<(long, long)> dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    private readonly List<(long, long)> dirsAll = [(1, -1), (-1, -1), (1, 1), (-1, 1), (1, 0), (-1, 0), (0, 1), (0, -1)];
+    private readonly List<(long, long)> dirsDiag = [(1, -1), (-1, -1), (1, 1), (-1, 1)];
+    private readonly long maxI;
+    private readonly long maxJ;
+
+    public Day05()
+    {
+        _input =
+        [
+            .. File.ReadAllText(string.Concat(InputFilePath.AsSpan(0, InputFilePath.Length - 4)
+//, "test"
+, ".txt")
+                        ).Split("\r\n\r\n")
+,
+        ];
+        Regex re = new(@"^(\d+)-(\d+)$");
+        _freshRanges = _input[0].Split("\r\n")
+            .Select(s => re.Match(s).Groups)
+            .Select(g => (long.Parse(g[1].Value), long.Parse(g[2].Value)))
+            .ToList();
+
+        _ids = _input[1].Split("\r\n")
+            .Select(long.Parse)
+            .ToList();
+    }
+    public override ValueTask<string> Solve_1()
+    {
+        long result = 0;
+        foreach (long id in _ids)
+        {
+            foreach ((long start, long end) in _freshRanges)
+            {
+                if (start <= id && id <= end)
+                {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+        return new ValueTask<string>($"Solution to {ClassPrefix} {result}, part 1");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        long result = 0;
+        List<(long start, long end)> orderedFreshRanges = _freshRanges.OrderBy(x => x.start).ToList();
+        long endLastRange = -1;
+        foreach ((long start, long end) in orderedFreshRanges)
+        {
+            long newStart = start > endLastRange ? start : endLastRange + 1;
+            if (newStart <= end)
+            {
+                result += end - newStart + 1;
+            }
+            endLastRange = Math.Max(endLastRange, end);
+        }
+        return new ValueTask<string>($"Solution to {ClassPrefix} {result}, part 2");
+    }
+
+    private string DictToString(Dictionary<(int, int), char> dict)
+    {
+        string r = "";
+        for (int i = 0; i < maxI; i++)
+        {
+            for (int j = 0; j < maxJ; j++)
+            {
+                if (dict.ContainsKey((i, j)))
+                {
+                    r += dict[(i, j)];
+                }
+                else
+                {
+                    r += '.';
+                }
+            }
+            r += "\r\n";
+        }
+        return r;
+    }
+
+    private string LocationSetToString(HashSet<(int, int)> energized)
+    {
+        string r = "";
+        for (int i = 0; i < maxI; i++)
+        {
+            for (int j = 0; j < maxJ; j++)
+            {
+                if (energized.Contains((i, j)))
+                {
+                    r += "#";
+                }
+                else
+                {
+                    r += '.';
+                }
+            }
+            r += "\r\n";
+        }
+        return r;
+    }
+
+    private void L(object o)
+    {
+        Console.WriteLine(o.ToString());
+    }
+
+    private void RegexYouKnow()
+    {
+        string temps =
+@"move 1 to A
+move 2 to B
+move 3 to C";
+        Regex re = new(@"^move (\d) to ([A-Z])$");
+        temps.Split("\r\n")
+            .Select(s => re.Match(s).Groups)
+            .Select(g => (int.Parse(g[1].Value), g[2].Value))
+            .ToList().ForEach(t => Console.WriteLine(t.Value));
+    }
+
+    private (long, long) Add((long, long) a, (long, long) b)
+    {
+        return (a.Item1 + b.Item1, a.Item2 + b.Item2);
+    }
+
+    private (long, long) Mul((long, long) a, long x)
+    {
+        return (a.Item1 * x, a.Item2 * x);
+    }
+    private bool InRange((long, long) a)
+    {
+        return a.Item1 >= 0 && a.Item2 >= 0 && a.Item1 < maxI && a.Item2 < maxJ;
+    }
+
+    private (long, long) Neg((long, long) p)
+    {
+        return (-p.Item1, -p.Item2);
+    }
+}
